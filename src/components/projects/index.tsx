4bd440a84abdb7125505098d4eb93cb69aa4d43b
Hrithik Sanyal <66126358+iamhtk@users.noreply.github.com>
2026-06-29 23:31:49 -0700

    perf: lazy load all project components and page components--reverted back

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,5 +1,18 @@
 // This file maps project IDs to their components and defines homepage project order
-import React, { lazy } from 'react';
+import React from 'react';
+import { AutomotiveUX_GMProject } from './AutomotiveUX_GM';
+import { CWPCProject } from './CWPC';
+import { RaseetHealthProject } from './RaseetHealth';
+import { BMWProject } from './BMW';
+import { CalmiRingProject } from './CalmiRing';
+import { BoundProject } from './bound';
+import { WeddingBlissProject } from './WeddingBliss';
+import { PortfolioDesignSystemProject } from './PortfolioDesignSystem';
+import { CwpcDsCaseStudyProject } from './CwpcDsCaseStudy';
+import { RaseetHealthDsCaseStudyProject } from './RaseetHealthDsCaseStudy';
+import { DesiPizzaHouseProject } from './DesiPizzaHouse';
+import { TandooriJunctionProject } from './TandooriJunction';
+import { CoyaxDesignSystemProject } from './CoyaxDesignSystem';
 
 export { PROJECT_ORDER } from './projectOrder';
 
@@ -8,18 +21,18 @@ export interface ProjectComponentProps {
   onProjectClick?: (projectId: string) => void;
 }
 
-export const projectComponents: Record<string, React.LazyExoticComponent<React.ComponentType<ProjectComponentProps>>> = {
-  CWPC: lazy(() => import('./CWPC').then(m => ({ default: m.CWPCProject }))),
-  AutomotiveUX_GM: lazy(() => import('./AutomotiveUX_GM').then(m => ({ default: m.AutomotiveUX_GMProject }))),
-  RaseetHealth: lazy(() => import('./RaseetHealth').then(m => ({ default: m.RaseetHealthProject }))),
-  BMW: lazy(() => import('./BMW').then(m => ({ default: m.BMWProject }))),
-  CalmiRing: lazy(() => import('./CalmiRing').then(m => ({ default: m.CalmiRingProject }))),
-  bound: lazy(() => import('./bound').then(m => ({ default: m.BoundProject }))),
-  WeddingBliss: lazy(() => import('./WeddingBliss').then(m => ({ default: m.WeddingBlissProject }))),
-  PortfolioDesignSystem: lazy(() => import('./PortfolioDesignSystem').then(m => ({ default: m.PortfolioDesignSystemProject }))),
-  CWPC_DS: lazy(() => import('./CwpcDsCaseStudy').then(m => ({ default: m.CwpcDsCaseStudyProject }))),
-  RaseetHealth_DS: lazy(() => import('./RaseetHealthDsCaseStudy').then(m => ({ default: m.RaseetHealthDsCaseStudyProject }))),
-  BuiltDeployed_Project1: lazy(() => import('./DesiPizzaHouse').then(m => ({ default: m.DesiPizzaHouseProject }))),
-  BuiltDeployed_Project2: lazy(() => import('./TandooriJunction').then(m => ({ default: m.TandooriJunctionProject }))),
-  CoyaxDesignSystem: lazy(() => import('./CoyaxDesignSystem').then(m => ({ default: m.CoyaxDesignSystemProject }))),
+export const projectComponents: Record<string, React.ComponentType<ProjectComponentProps>> = {
+  CWPC: CWPCProject,
+  AutomotiveUX_GM: AutomotiveUX_GMProject,
+  RaseetHealth: RaseetHealthProject,
+  BMW: BMWProject,
+  CalmiRing: CalmiRingProject,
+  bound: BoundProject,
+  WeddingBliss: WeddingBlissProject,
+  PortfolioDesignSystem: PortfolioDesignSystemProject,
+  CWPC_DS: CwpcDsCaseStudyProject,
+  RaseetHealth_DS: RaseetHealthDsCaseStudyProject,
+  BuiltDeployed_Project1: DesiPizzaHouseProject,
+  BuiltDeployed_Project2: TandooriJunctionProject,
+  CoyaxDesignSystem: CoyaxDesignSystemProject,
 };
